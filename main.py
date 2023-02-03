0073a47e3cf82e710ef1eb2e52c8f93f03ef3d13
Zafer Akçalı <zaferakcali@yahoo.com>
2023-02-03 14:54:22 +0300

    use request instead of session

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
-# genelpara api V1.2, by Zafer Akçalı
-from requests import Session #pip install requests
+# genelpara api V1.4, by Zafer Akçalı
+import urllib.request
+import urllib.error
 import certifi #pip install certifi, to be sure to pass ssl verification in every URL
 import time
+import ssl
+import json
 from decimal import Decimal
 
 API_URL = 'https://api.genelpara.com/embed/borsa.json'
 HEADERS={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-WHERE_CA= certifi.where()
-TIME_DELAY = 60
-session = Session()
+TIME_DELAY = 10*60
+CA_WHERE=certifi.where()
+SSL_CONTEXT = ssl.create_default_context(cafile=CA_WHERE)
 def get_response ():
-    response = session.get (API_URL, headers=HEADERS, verify=WHERE_CA)
-    if response.status_code != 200:
-        print ("Connection error:", response.status_code)
-        exit ()
-    apiDict=response.json()
-    USD=Decimal(apiDict ['USD']['satis'])
-    EUR=Decimal(apiDict ['EUR']['satis'])
-    XU100=Decimal(apiDict ['XU100']['satis'])
-    BTC=Decimal(apiDict ['BTC']['satis'])
-    ETH=Decimal(apiDict ['ETH']['satis'])
+    try:
+        req=urllib.request.Request (API_URL, headers=HEADERS)
+        resp=urllib.request.urlopen(req, context=SSL_CONTEXT)
+    except urllib.error.HTTPError as e:
+        print ("HTTPError:", e)
+        exit()
+    except urllib.error.URLError as e:
+        print ("URLError:", e)
+        exit()
+    byte_code=resp.read().decode()
+    api_dict=json.loads(byte_code)
+    USD=Decimal(api_dict ['USD']['satis'])
+    EUR=Decimal(api_dict ['EUR']['satis'])
+    XU100=Decimal(api_dict ['XU100']['satis'])
+    BTC=Decimal(api_dict ['BTC']['satis'])
+    ETH=Decimal(api_dict ['ETH']['satis'])
     return (USD,EUR,XU100,BTC,ETH)
 
 last_usd,last_eur,last_xu100,last_btc,last_eth = get_response()
